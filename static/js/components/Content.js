00e89f9ef69aa99127d16ce76e855379a14e0b8a
Justin Harwood <harwoodjp@g.cofc.edu>
2017-03-30 13:01:35 -0400

    fixed sidebar, scroll content area

--- static/js/components/Content.js
+++ static/js/components/Content.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import styled from "styled-components"
+
+const Wrapper = styled.div`
+	background:#FFA953;
+	width:75%;
+	height:150%;
+	position:relative;
+	left:25%;
+`;
 
 class Content extends React.Component {
 	render() {
 		return( 
-			<p>world</p>				    
+			<Wrapper>
+				Content
+			</Wrapper>				    
 		)			
 	}
 }
--- static/js/components/Sidebar.js
+++ static/js/components/Sidebar.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import styled from "styled-components"
+
+const Wrapper = styled.div`
+	background:#E99E9E;
+	width:25%;
+	height:100%;
+	position:fixed;
+`;
 
 class Sidebar extends React.Component {
 	render() {
 		return( 		    
-			<p>hello</p>
+			<Wrapper>
+				Sidebar
+			</Wrapper>
 		)
 	}
 }
